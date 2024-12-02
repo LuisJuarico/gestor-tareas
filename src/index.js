495a6bcb5721cae97ffa2c3bbfdccac2f4fbeb96
Luis Juarico <luis.221271@gmail.com>
2024-12-02 15:19:44 -0600

    Añadir soporte PWA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(
+      (registration) => {
+        console.log('Service Worker registrado con éxito:', registration);
+      },
+      (error) => {
+        console.log('Error al registrar el Service Worker:', error);
+      }
+    );
+  });
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
